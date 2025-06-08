b3ef47907bd816a50e7ea08d8334b03e6bd37a11
Theophilus Asamoah Gyapong <jhapson34@gmail.com>
2025-06-08 20:19:15 +0000

    fetch question data api, conert html entities to original

---
+++ ques_and_ans.py
@@ -0,0 +1,28 @@
+
+import html
+from random import shuffle
+
+import requests
+
+quiz_parameters = {
+    "amount": 15,
+    "category": 18,
+    "difficulty": "hard",
+    "type": "multiple",
+}
+
+quiz_response = requests.get(
+    url='https://opentdb.com/api.php', params=quiz_parameters)
+quiz_response.raise_for_status()
+
+
+ques_ans_dataa = quiz_response.json()['results']
+
+# add correct ans to incorrect to get full quiz answer options
+for a_dict in ques_ans_dataa:
+    (a_dict['incorrect_answers']).append(a_dict['correct_answer'])
+    shuffle(a_dict['incorrect_answers'])
+
+    # html unescape to change html entites back to original format
+    html.unescape(a_dict['question'])
+ques_ans_data = ques_ans_dataa

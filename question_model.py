7ea75b177018c5f6562599e36b5aed5a97e07c04
Theophilus Asamoah Gyapong <jhapson34@gmail.com>
2025-06-08 20:20:49 +0000

    the questions' model

---
+++ question_model.py
@@ -0,0 +1,8 @@
+# Quiz game questions modelling
+
+class QuestionModel:
+
+    def __init__(self, question, answer, options):
+        self.ques = question
+        self.ans = answer
+        self.options = options
